23ca92c68f0725ba6653753870a540a3c3b2eba5
nmfirdausw <nmfirdausw@gmail.com>
2025-02-14 09:47:45 +0800

    feat: add neovide alias

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     set -g fish_greeting
     alias ls="eza"
     zoxide init fish | source
+
+    alias nvide="neovide --fork --size=2735x1707 --frame=buttonless --srgb --title-hidden"
 end
 
 fzf --fish | source
